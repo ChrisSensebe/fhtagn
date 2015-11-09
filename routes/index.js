44c7d52f0044c3b42b4a39eaee9aa1371a7987ab
ChrisSensebe <christophe.sensebe@gmail.com>
2015-11-09 14:01:54 +0100

    adding routes & views for tags @ archives pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,33 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Fhtagn' });
 });
 
+/* GET tags page. */
+router.get('/tags', function(req ,res, next){
+	res.render('tags', { title: 'Fhtagn' });
+});
+
+/* GET archives page */
+router.get('/archives', function(req ,res, next){
+	res.render('archives', { title: 'Fhtagn' });
+});
+
+/* Protected routes */
+
+/* GET adminHome */
+/* GET createNewPost */
+/* GET editPostById */
+/* GET usersPage */
+/* GET createUser */
+/* GET editUserById */
+/* GET uploadFile */
+/* GET customize */ 
+/* GET login */
+/* GET signup */
+/* POST createNewPost */
+/* POST createUser */
+/* POST editUserByID */
+/* POST customize */
+/* POST login */
+/* POST signup */
+
 module.exports = router;
