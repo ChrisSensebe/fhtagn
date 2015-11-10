e8b091dcc2404bfd7afec2596f0bd55c6bbb075c
ChrisSensebe <christophe.sensebe@gmail.com>
2015-11-10 16:02:23 +0100

    finished site routes

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,13 @@ router.get('/admin/signup', function(req ,res, next){
 	res.render('signup', { title: 'Fhtagn | admin' });
 });
 /* POST login */
+router.post('/admin/login', function(req, res, next){
+	res.redirect('/admin', { title: 'Fhtagn | admin'});
+});
 /* POST signup */
+router.post('/admin/signup', function(req, res, next){
+	res.redirect('/admin', { title: 'Fhtagn | admin'});
+});
 
 /* Protected routes */
 
@@ -38,6 +44,9 @@ router.get('/admin/newPost', function(req ,res, next){
 	res.render('newPost', { title: 'Fhtagn | admin' });
 });
 /* GET editPostById */
+router.get('/admin/editPostById', function(req ,res, next){
+	res.render('editPostById', { title: 'Fhtagn | editPostById' });
+});
 /* GET usersPage */
 router.get('/admin/users', function(req ,res, next){
 	res.render('users', { title: 'Fhtagn | admin' });
@@ -47,6 +56,9 @@ router.get('/admin/createUser', function(req ,res, next){
 	res.render('createUser', { title: 'Fhtagn | admin' });
 });
 /* GET editUserById */
+router.get('/admin/editUserById', function(req ,res, next){
+	res.render('editPostById', { title: 'Fhtagn | editPostById' });
+});
 /* GET uploadFile */
 router.get('/admin/uploadfile', function(req ,res, next){
 	res.render('uploadFile', { title: 'Fhtagn | admin' });
@@ -56,9 +68,28 @@ router.get('/admin/themes', function(req ,res, next){
 	res.render('themes', { title: 'Fhtagn | admin' });
 });
 /* POST createNewPost */
+router.post('/admin/newPost', function(req, res, next){
+	res.redirect('/admin', { title: 'Fhtagn | admin'});
+});
 /* POST createUser */
+router.post('/admin/newUser', function(req, res, next){
+	res.redirect('/admin', { title: 'Fhtagn | admin'});
+});
+/* POST editPostById */
+router.post('/admin/editPostById', function(req ,res, next){
+	res.render('adminHome', { title: 'Fhtagn | admin' });
+});
 /* POST editUserByID */
+router.get('/admin/editUserById', function(req ,res, next){
+	res.render('editUserById', { title: 'Fhtagn | admin' });
+});
 /* POST uploadFile */
-/* POST customize */
+router.post('/admin/uploadFile', function(req, res, next){
+	res.redirect('/admin', { title: 'Fhtagn | admin'});
+});
+/* POST Themes */
+router.post('/admin/themes', function(req, res, next){
+	res.redirect('/admin', { title: 'Fhtagn | admin'});
+});
 
 module.exports = router;
