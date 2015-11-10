dce0b630f362ae4344055fbf63637e878df5cb78
ChrisSensebe <christophe.sensebe@gmail.com>
2015-11-10 13:43:01 +0100

    creating admin views

--- routes/index.js
+++ routes/index.js
@@ -16,24 +16,49 @@ router.get('/archives', function(req ,res, next){
 	res.render('archives', { title: 'Fhtagn' });
 });
 
+/* GET login */
+router.get('/admin/login', function(req ,res, next){
+	res.render('login', { title: 'Fhtagn | admin' });
+});
+/* GET signup */
+router.get('/admin/signup', function(req ,res, next){
+	res.render('signup', { title: 'Fhtagn | admin' });
+});
+/* POST login */
+/* POST signup */
+
 /* Protected routes */
 
 /* GET adminHome */
-/* GET createNewPost */
+router.get('/admin/', function(req ,res, next){
+	res.render('adminHome', { title: 'Fhtagn | admin' });
+});
+/* GET newPost */
+router.get('/admin/newPost', function(req ,res, next){
+	res.render('newPost', { title: 'Fhtagn | admin' });
+});
 /* GET editPostById */
 /* GET usersPage */
+router.get('/admin/users', function(req ,res, next){
+	res.render('users', { title: 'Fhtagn | admin' });
+});
 /* GET createUser */
+router.get('/admin/createUser', function(req ,res, next){
+	res.render('createUser', { title: 'Fhtagn | admin' });
+});
 /* GET editUserById */
 /* GET uploadFile */
-/* GET customize */ 
-/* GET login */
-/* GET signup */
+router.get('/admin/uploadfile', function(req ,res, next){
+	res.render('uploadFile', { title: 'Fhtagn | admin' });
+});
+/* GET themes */ 
+router.get('/admin/themes', function(req ,res, next){
+	res.render('themes', { title: 'Fhtagn | admin' });
+});
 /* POST createNewPost */
 /* POST createUser */
 /* POST editUserByID */
 /* POST uploadFile */
 /* POST customize */
-/* POST login */
-/* POST signup */
 
 module.exports = router;
