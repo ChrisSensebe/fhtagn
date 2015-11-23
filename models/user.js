740107fc306203d0448092bfaf0d913fc3b2b533
ChrisSensebe <christophe.sensebe@gmail.com>
2015-11-23 20:32:52 +0100

    adding user schema

--- models/user.js
+++ models/user.js
@@ -1,1 +1,39 @@
-// todo
+var mongoose = require('../mongoose');
+var bcrypt   = require('../bcrypt');
+var config   = require('../config.js');
+
+var userSchema = mongoose.Schema({
+	username     : String,
+	email        : String,
+	passwordhash : String,
+	role         : String
+});
+
+userSchema.set('autoIndex', false);
+
+// pre save function, salt & has user password
+userSchema.pre('save', function(next){
+	var user = this;
+	if(!user.isModified('passwordhash')){
+		return next();
+	}
+	bcrypt.genSalt(config.saltWorkFactor, function(err, salt){
+		if(err){
+			return next(err);
+		}
+		bcrypt.hash(user.passwordhash, salt, function(err, hash){
+			if(err){
+				return next(err);
+			}
+			user.passwordhash = hash;
+			next();
+		});
+	});
+});
+
+// check if valid password
+schema.methods.isValidPassword = function(password){
+	return bcrypt.compareSync(password, this.passwordhash);
+}
+
+module.exports = mongoose.model('User', userSchema);
