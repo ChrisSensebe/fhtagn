f1666a69a564ffb59a3af81c39196f1c569c1634
ChrisSensebe <christophe.sensebe@gmail.com>
2015-11-28 13:48:23 +0100

    finished local-signup in passportConfig & adding connect-flash to app

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -22,6 +22,39 @@ module.exports = function(passport){
         });
     });
 
+    /**
+     * local signup
+     */
+    passport.use('local-signup', new LocalStrategy({
+        usernameField     : 'username',
+        passwordField     : 'password',
+        passReqToCallback : true
+    }, function(req, email, password, next){
+
+        process.nextTick(function(){
+
+            User.findOne({'email' : email}, function(err, user){
+
+                if(err){
+                    return next(err);
+                } else if(user){
+                    return done(null, false, req.flash('signupMessage', 'That user is already in use'));
+                } else {
+                    var newUser = new user();
+                    newUser.passwordHash = password;
+
+                    newUser.save(function(err){
+                        if(err){
+                            return next(err);
+                        }
+                        return done(null, newUser);
+                    })
+                }
+            })
+
+        })
+
+    }));
 }
 
 
