1ab4e4b327fd648aacd03a6078e57b523ac47e9e
ChrisSensebe <christophe.sensebe@gmail.com>
2015-12-12 18:42:51 +0100

    attaching current user to res.locals

--- middlewares/attachAuthentication.js
+++ middlewares/attachAuthentication.js
@@ -4,6 +4,12 @@
 
 // attach authentication to response object
 module.exports = function(req, res, next){
+
     res.locals.isAuthenticated = req.isAuthenticated();
+
+    if(req.isAuthenticated()){
+        res.locals.currentUser = req.user;
+    }
+
     next();
 }
