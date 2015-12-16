edb590e05799531bb616e3d2ed7294b828a45f4f
ChrisSensebe <christophe.sensebe@gmail.com>
2015-12-16 23:14:46 +0100

    adding button to close flash messages & transition effect

--- public/scripts/front.js
+++ public/scripts/front.js
@@ -1,3 +1,10 @@
 /**
  * Created by csensebe on 16/12/2015.
  */
+$(function(){
+    // add class to hide flash messages
+    $('.hide-flash').click(function(){
+        $(this).parent().addClass('hide-message');
+        $(this).parents().eq(1).addClass('shrink-element');
+    });
+});
