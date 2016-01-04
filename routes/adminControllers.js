f521241e2763ae3c0f65e00640bb55e5e0227a07
ChrisSensebe <christophe.sensebe@gmail.com>
2016-01-04 22:05:21 +0100

    moving admin controllers to adminControllers.js

--- routes/adminControllers.js
+++ routes/adminControllers.js
@@ -1,3 +1,230 @@
-/**
- * Created by csensebe on 26/12/2015.
- */
+var Post     = require('../models/post.js');
+var siteConf = require('../config/siteConfig.js');
+
+// find all post in database, render admin homepage
+exports.getAdminHome = function(req, res){
+    Post.find().sort('-created').exec(function(err, docs){
+        if(err){
+            return next(err);
+        }
+        res.render('adminViews/adminHome', {
+            title : siteConf.adminConfig.siteTitle,
+            pageTitle : siteConf.adminConfig.homePage.pageTitle,
+            posts : docs
+        });
+    });
+};
+// set goodbye flash message, log user out, redirect to site homepage
+exports.getLogout = function(req, res){
+    req.flash('success', 'bye');
+    req.logout();
+    res.redirect('/');
+};
+// render create newpost page
+exports.getNewPost = function(req, res){
+    res.render('adminViews/createNewPost', {
+        title : siteConf.adminConfig.siteTitle,
+        pageTitle : siteConf.adminConfig.newPostPage.pageTitle
+    });
+};
+// save new post in database
+exports.postNewPost = function(req, res, next){
+    // get info from form
+    var title  = req.body.title;
+    var post   = req.body.post;
+    var author = req.user.username;
+    var tags   = req.body.tags.split(',');
+    // create new post
+    var newPost = Post({
+        title   : title,
+        post    : post,
+        author  : author,
+        tags    : tags,
+        created : Date.now(),
+        updated : Date.now()
+    });
+    // save post in database
+    newPost.save(function(err){
+        if(err){
+            req.flash('danger', 'error saving post in database');
+            return next(err);
+        }
+    });
+    // all went well, set flash message, redirect to admin home
+    req.flash('success', 'Post successfully saved.');
+    res.redirect('/admin/');
+};
+// get post by id in database, render edit post page
+exports.getEditPostById = function(req ,res, next){
+    // get post id from form
+    var id = req.params.id;
+    // find post in database, render edit post page
+    Post.findOne({_id : id}, function(err, doc){
+        if(err){
+            return next(err);
+        }
+        res.render('adminViews/editPost', {
+            title : siteConf.adminConfig.siteTitle,
+            post : doc
+        });
+    });
+};
+// save post in database
+exports.postSavePost = function(req ,res, next){
+    // get info from form
+    var id      = req.body.id;
+    var title   = req.body.title;
+    var post    = req.body.post;
+    var tags    = req.body.tags.split(',');
+    // find post in database
+    Post.findOne({_id : id}, function(err, doc){
+
+        if(err){
+            req.flash('danger', 'Error saving post in database');
+            return next(err);
+        }
+        // update post object
+        doc.title   = title;
+        doc.post    = post;
+        doc.tags    = tags;
+        doc.updated = Date.now();
+        // save post in database, set flash message, redirect to admin homepage
+        doc.save(function(err){
+
+            if(err){
+                req.flash('danger', 'Error saving post in database');
+                return next(err);
+            }
+            req.flash('success', 'Post successfully updated');
+            res.redirect('/admin/');
+        });
+    });
+};
+// find and delete post from database
+exports.postDelPost = function(req, res){
+    res.redirect('/admin/');
+};
+// find users in database, render users page
+exports.getUsers = function(req, res, next){
+    // find all users in database, render users page
+    User.find().exec(function(err, docs){
+        if(err){
+            return next(err);
+        }
+        res.render('adminViews/users', {
+            title : siteConf.adminConfig.siteTitle,
+            pageTitle : siteConf.adminConfig.usersPage.pageTitle,
+            users : docs
+        });
+    });
+};
+// render new user page
+exports.getNewUser = function(req, res){
+    // render new user page
+    res.render('adminViews/newUser', {
+        title : siteConf.adminConfig.siteTitle,
+        pageTitle : siteConf.adminConfig.newUserPage.pageTitle
+    });
+};
+// get user in database, render user page
+exports.getUserById = function(req, res, next){
+    // get user id from url
+    var userId = req.params.id;
+    // find user in database, render edit user page
+    User.findOne({_id : userId}, function(err, doc){
+        if(err){
+            return next(err);
+        }
+        res.render('adminViews/user', {
+            title : siteConf.adminConfig.siteTitle,
+            user : doc
+        });
+    });
+};
+// save new user in database, redirect to users page
+exports.postSaveNewUser = function(req, res){
+    // get user info from form
+    var username = req.body.username;
+    var email    = req.body.email;
+    var password = req.body.password;
+    // create new user
+    var newUser = new User({
+        username     : username,
+        email        : email,
+        passwordHash : password,
+        role         : 'peon'
+    });
+    // save user in database, set flash messages, redirect to users
+    newUser.save(function(err){
+        if(err){
+            if(err){
+                req.flash('danger', 'Error saving user in database');
+                return next(err);
+            }
+            req.flash('success', 'User successfully saved');
+            res.redirect('/admin/users');
+        }
+    });
+};
+// find user in database, update user, redirect to users page
+exports.postSaveUser = function(req, res, next){
+    // get user info from form
+    var id       = req.body.id;
+    var username = req.body.username;
+    var email    = req.body.email;
+    if(req.body.password){
+        var password = req.body.password;
+    }
+    // find user in database
+    User.findOne({_id : id}, function(err, doc){
+        if(err){
+            return next(err);
+        }
+        // update user
+        doc.username = username;
+        doc.email    = email;
+        doc.updated  = Date.now();
+        if(password){
+            doc.passwordHash = password;
+        }
+        // save user in database
+        doc.save(function(err){
+            if(err){
+                req.flash('danger', 'Error saving user in database');
+                return next(err);
+            }
+        });
+        req.flash('success', 'User successfully saved');
+        res.redirect('/admin/users');
+    });
+};
+// del user from database, redirect to users
+exports.postDelUser = function(req, res){
+    res.redirect('/admin/users');
+};
+// render files page
+exports.getFiles = function(req, res){
+    res.render('adminViews/files', {
+        title : siteConf.adminConfig.siteTitle,
+        pageTitle : siteConf.adminConfig.filesPage.pageTilte
+    });
+};
+// upload file, redirect to files
+exports.postUpload = function(req, res){
+    res.redirect('/admin/files');
+};
+// delete file, redirect to files
+exports.postDelFile = function(req, res){
+    res.redirect('/admin/files');
+};
+// render theme page
+exports.getTheme = function(req, res){
+    res.render('adminViews/theme', {
+        title : siteConf.adminConfig.siteTitle,
+        pageTitle : siteConf.adminConfig.themePage.pageTitle
+    });
+};
+// save theme in db, redirect to them
+exports.postSaveTheme = function(req, res){
+    res.redirect('/admin/theme');
+};
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ var User             = require('../models/user.js');
 var Post             = require('../models/post.js');
 var passport         = require('passport');
 var isLogged         = require('../middlewares/isLogged.js');
-var siteConf         = require('../config/siteConfig.js');
 
 /**
  * site routes
@@ -25,240 +24,28 @@ router.post('/login',   passport.authenticate('local-login', {
 }));
 
 /**
- * protected routes user must be logged to acces route
+ * admin routes
+ * protected routes user must be logged
  */
-router.all('/admin/*', isLogged);
 
-// get logout
-router.get('/admin/logout', function(req, res){
-    // set goodbye flash message, log user out, redirect to site homepage
-    req.flash('success', 'bye');
-    req.logout();
-	res.redirect('/');
-});
-// get admin homepage
-router.get('/admin/', function(req ,res){
-    // find all post in database, render admin homepage
-    Post.find().sort('-created').exec(function(err, docs){
-        if(err){
-            return next(err);
-        }
-        res.render('adminViews/adminHome', {
-            title : siteConf.adminConfig.siteTitle,
-            pageTitle : siteConf.adminConfig.homePage.pageTitle,
-            posts : docs
-        });
-    });
-});
-// get new post page
-router.get('/admin/newPost', function(req, res){
-    // render create newpost page
-	res.render('adminViews/createNewPost', {
-        title : siteConf.adminConfig.siteTitle,
-        pageTitle : siteConf.adminConfig.newPostPage.pageTitle
-    });
-});
-// post save new post form
-router.post('/admin/saveNewPost', function(req, res, next){
-
-    // get info from form
-    var title  = req.body.title;
-    var post   = req.body.post;
-    var author = req.user.username;
-    var tags   = req.body.tags.split(',');
-    // create new post
-    var newPost = Post({
-        title   : title,
-        post    : post,
-        author  : author,
-        tags    : tags,
-        created : Date.now(),
-        updated : Date.now()
-    });
-    // save post in database
-    newPost.save(function(err){
-        if(err){
-            req.flash('danger', 'error saving post in database');
-            return next(err);
-        }
-    });
-    // all went well, set flash message, redirect to admin home
-    req.flash('success', 'Post successfully saved.');
-    res.redirect('/admin/');
-});
-// get edit post by id
-router.get('/admin/post/:id', function(req ,res, next){
-    // get post id from form
-    var id = req.params.id;
-    // find post in database, render edit post page
-    Post.findOne({_id : id}, function(err, doc){
-        if(err){
-            return next(err);
-        }
-        res.render('adminViews/editPost', {
-            title : siteConf.adminConfig.siteTitle,
-            post : doc
-        });
-    });
-});
-// post save post form
-router.post('/admin/savePost', function(req ,res, next){
-    // get info from form
-    var id      = req.body.id;
-    var title   = req.body.title;
-    var post    = req.body.post;
-    var tags    = req.body.tags.split(',');
-    // find post in database
-    Post.findOne({_id : id}, function(err, doc){
-
-        if(err){
-            req.flash('danger', 'Error saving post in database');
-            return next(err);
-        }
-        // update post object
-        doc.title   = title;
-        doc.post    = post;
-        doc.tags    = tags;
-        doc.updated = Date.now();
-        // save post in database, set flash message, redirect to admin homepage
-        doc.save(function(err){
-
-            if(err){
-                req.flash('danger', 'Error saving post in database');
-                return next(err);
-            }
-            req.flash('success', 'Post successfully updated');
-            res.redirect('/admin/');
-        });
-    });
-});
-// post del post
-router.post('/admin/delPost', function(req, res){
-	res.redirect('/admin/');
-});
-// get all users page
-router.get('/admin/users', function(req, res, next){
-    // find all users in database, render users page
-    User.find().exec(function(err, docs){
-        if(err){
-            return next(err);
-        }
-        res.render('adminViews/users', {
-            title : siteConf.adminConfig.siteTitle,
-            pageTitle : siteConf.adminConfig.usersPage.pageTitle,
-            users : docs
-        });
-    });
-});
-// get newUser page
-router.get('/admin/newUser', function(req, res){
-    // render new user page
-    res.render('adminViews/newUser', {
-        title : siteConf.adminConfig.siteTitle,
-        pageTitle : siteConf.adminConfig.newUserPage.pageTitle
-    });
-});
-// get user page by id
-router.get('/admin/user/:id', function(req, res, next){
-    // get user id from url
-    var userId = req.params.id;
-    // find user in database, render edit user page
-    User.findOne({_id : userId}, function(err, doc){
-        if(err){
-            return next(err);
-        }
-        res.render('adminViews/user', {
-            title : siteConf.adminConfig.siteTitle,
-            user : doc
-        });
-    });
-});
-//post save new user
-router.post('/admin/saveNewUser', function(req, res){
-    // get user info from form
-    var username = req.body.username;
-    var email    = req.body.email;
-    var password = req.body.password;
-    // create new user
-    var newUser = new User({
-        username     : username,
-        email        : email,
-        passwordHash : password,
-        role         : 'peon'
-    });
-    // save user in database, set flash messages, redirect to users
-    newUser.save(function(err){
-        if(err){
-            if(err){
-                req.flash('danger', 'Error saving user in database');
-                return next(err);
-            }
-            req.flash('success', 'User successfully saved');
-            res.redirect('/admin/users');
-        }
-    });
-});
-// post save user form
-router.post('/admin/saveUser', function(req, res, next){
-    // get user info from form
-    var id       = req.body.id;
-	var username = req.body.username;
-    var email    = req.body.email;
-    if(req.body.password){
-        var password = req.body.password;
-    }
-    // find user in database
-    User.findOne({_id : id}, function(err, doc){
-        if(err){
-            return next(err);
-        }
-        // update user
-        doc.username = username;
-        doc.email    = email;
-        doc.updated  = Date.now();
-        if(password){
-            doc.passwordHash = password;
-        }
-        // save user in database
-        doc.save(function(err){
-            if(err){
-                req.flash('danger', 'Error saving user in database');
-                return next(err);
-            }
-        });
-        req.flash('success', 'User successfully saved');
-        res.redirect('/admin/users');
-    });
-});
-// post del user form
-router.post('/admin/delUser', function(req, res){
-	res.redirect('/admin/users');
-});
-// get admin upload file file
-router.get('/admin/files', function(req, res){
-	res.render('adminViews/files', {
-        title : siteConf.adminConfig.siteTitle,
-        pageTitle : siteConf.adminConfig.filesPage.pageTilte
-    });
-});
-// post save file
-router.post('/admin/upload', function(req, res){
-	res.redirect('/admin/files');
-});
-// del file
-router.post('/admin/delFile', function(req, res){
-	res.redirect('/admin/files');
-});
-// get admin themes page
-router.get('/admin/theme', function(req, res){
-	res.render('adminViews/theme', {
-        title : siteConf.adminConfig.siteTitle,
-        pageTitle : siteConf.adminConfig.themePage.pageTitle
-    });
-});
-// post change theme
-router.post('/admin/theme', function(req, res){
-	res.redirect('/admin/theme');
-});
+router.all('/admin/*',            isLogged);
+router.get('/admin/',             adminControllers.getAdminHome);
+router.get('/admin/newPost',      adminControllers.getNewPost);
+router.get('/admin/post/:id',     adminControllers.getEditPostById);
+router.get('/admin/users',        adminControllers.getUsers);
+router.get('/admin/newUser',      adminControllers.getNewUser);
+router.get('/admin/user/:id',     adminControllers.getUserById);
+router.get('/admin/files',        adminControllers.getFiles);
+router.get('/admin/theme',        adminControllers.getTheme);
+router.get('/admin/logout',       adminControllers.getLogout);
+router.post('/admin/saveNewPost', adminControllers.postNewPost);
+router.post('/admin/savePost',    adminControllers.postSavePost);
+router.post('/admin/delPost',     adminControllers.postDelPost);
+router.post('/admin/saveNewUser', adminControllers.postSaveNewUser);
+router.post('/admin/saveUser',    adminControllers.postSaveUser);
+router.post('/admin/delUser',     adminControllers.postDelUser);
+router.post('/admin/upload',      adminControllers.postUpload);
+router.post('/admin/delFile',     adminControllers.postDelFile);
+router.post('/admin/theme',       adminControllers.postSaveTheme);
 
 module.exports = router;
